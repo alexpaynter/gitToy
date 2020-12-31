b3fb5ebd8504ade13d992b18332b668e0b6d53e8
apGitTestr <apGitTestr@protonmail.com>
2020-12-31 01:44:01 -0800

    fixie

--- R/util.R
+++ R/util.R
@@ -1,7 +1,7 @@
-est_ci_str <- function(est, lci, uci, d = 2) {
+est_ci_str <- function(est, lci, uci, d = 2, cid = d) {
     est <- formatC(est, digits = d, format = 'f')
-    lci <- formatC(lci, digits = d, format = 'f')
-    uci <- formatC(uci, digits = d, format = 'f')
+    lci <- formatC(lci, digits = cid, format = 'f')
+    uci <- formatC(uci, digits = cid, format = 'f')
 
     return(paste0(est, " (", lci, ", ", uci, ")"))
 }
