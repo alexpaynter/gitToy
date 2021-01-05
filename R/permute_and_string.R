26e22fcdd7f28d2aa3ce9ad64ed6ed3c07e37727
alexpaynter <alexcpaynter@gmail.com>
2021-01-04 23:05:12 -0800

    added permute and string function

---
+++ R/permute_and_string.R
@@ -0,0 +1,13 @@
+#' @title Permute and string
+#'
+#' @description Randomly choose an order for a,b,c.  Return a comma-space
+#'   separated string.
+#'
+#' @details A completely pointless function - fitting for a pointless package!
+#' @export
+permute_and_string <- function(a,b,c) {
+    vec <- c(a,b,c)
+    vec <- sample(vec)
+    vec <- paste(vec, collapse = ", ")
+    return(vec)
+}
