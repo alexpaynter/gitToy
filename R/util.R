86b1a546cb0902e822adc84a65de91e3b875a9a2
apGitTestr <apGitTestr@protonmail.com>
2021-01-04 22:28:30 -0800

    Added docs

--- R/util.R
+++ R/util.R
@@ -1,7 +1,19 @@
-est_ci_str <- function(est, lci, uci, d = 2, cid = d) {
+#' @title Estimate and confidence interval string maker
+#'
+#' @description Compresses an estimate, lower (lci) and upper (uci) confidence interval into a string.
+#'
+#' @details cid is for confidence interval digits.  If it is omitted then d is used.  d is for the number of digits in the estimate.
+#'
+#' @param est The point estimate.
+#' @param lcb The lower confidence bound.
+#' @param ucb The upper confidence bound.
+#' @param d Digits for the estimate.
+#' @param cid Digits for the confidence interval (defaults to d)
+#' @export
+est_ci_str <- function(est, lcb, ucb, d = 2, cid = d) {
     est <- formatC(est, digits = d, format = 'f')
-    lci <- formatC(lci, digits = cid, format = 'f')
-    uci <- formatC(uci, digits = cid, format = 'f')
+    lcb <- formatC(lcb, digits = cid, format = 'f')
+    ucb <- formatC(ucb, digits = cid, format = 'f')
 
-    return(paste0(est, " (", lci, ", ", uci, ")"))
+    return(paste0(est, " (", lcb, ", ", ucb, ")"))
 }
