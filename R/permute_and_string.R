7f162d5aa4fa991336a9ddffb586717f6295f95f
alexpaynter <alexcpaynter@gmail.com>
2021-01-05 00:31:56 -0800

    Package check fixes

--- R/permute_and_string.R
+++ R/permute_and_string.R
@@ -4,6 +4,9 @@
 #'   separated string.
 #'
 #' @details A completely pointless function - fitting for a pointless package!
+#' @param a An item
+#' @param b Some other item
+#' @param c A third item
 #' @export
 permute_and_string <- function(a,b,c) {
     vec <- c(a,b,c)
